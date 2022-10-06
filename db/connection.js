e5e33f8f64143b62de475b9320f911faba9b68f7
Mattrp227 <109999331+Mattrp227@users.noreply.github.com>
2022-10-05 23:29:00 -0500

    Update connection.js

--- db/connection.js
+++ db/connection.js
@@ -1,1 +1,29 @@
+//initialize mysql2
+
+const mysql = require('mysql2');
+const util = require('util');
+
+//user and password
+
+let db_user = process.env.db_user;
+let db_pw = process.env.db_pw;
+
+
+//dotenv
+require('dotenv').config();
+
+
+//constructor for user and password connection
+const db = mysql.createConnection(
+    {
+
+        user: db_user,
+        password: db_pw,
+        database: 'employee_db',
+
+    },
+)
+
+//export
+module.exports = db;
 
