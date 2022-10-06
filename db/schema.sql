323dcc745f7eba089979fe1e09f1d749dd3faf1f
Mattrp227 <109999331+Mattrp227@users.noreply.github.com>
2022-10-05 23:28:36 -0500

    Update schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,15 @@
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
 
+USE employee_db;
+
+CREATE TABLE roles(
+    id INT NOT NULL,
+    title VARCHAR(30) NOT NULL,
+);
+
+CREATE TABLE employees (
+    id INT NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+);
