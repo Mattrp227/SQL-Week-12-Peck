fcee9c0a619f51da3db4c953ab6a64fe0a583a4a
Mattrp227 <109999331+Mattrp227@users.noreply.github.com>
2022-10-05 23:29:19 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
-
+//create initial constructors
+const db = require('./db/connection'); 
+const inquirer = require('inquirer');
+const figlet = require('figlet');
+require('console.table');
+//prompt inquirer
+const beginQuestions = () => {
+    return inquirer.createPromptModule([{
+        type: 'Question',
+        name: 'answers',
+        message: 'click an option',
+        choices: [
+            'Add Employee',
+            'Remove Employee',
+            'Add Role',
+            'Remove Role',
+            'Add Department',
+            'Remove Department',
+        ]
+    }])
+}
